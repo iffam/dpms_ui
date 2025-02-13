2a0fb54fa6266267ec4d00d71da961b917f7e06f
iffam <iffaam@gmail.com>
2025-02-14 03:14:37 +0500

    update AppComponent to integrate Angular Material components and remove unit tests

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'dpms';
+  drawerMode: 'over' | 'side' = 'side';
+  drawerOpened: boolean = true;
+  title = 'DPMS';
+
+  /**
+   * Constructor
+   */
+  constructor() {}
 }
