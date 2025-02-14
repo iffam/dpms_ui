e73b92a045b0fa52f1975d59d69e1ccc391bcd7e
iffam <iffaam@gmail.com>
2025-02-14 23:49:18 +0500

    feat: integrate ngx-scanner-qrcode for QR code scanning functionality

--- ui/src/app/modules/scan/scan.component.ts
+++ ui/src/app/modules/scan/scan.component.ts
@@ -1,8 +1,11 @@
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { LOAD_WASM, NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
+LOAD_WASM('assets/wasm/ngx-scanner-qrcode.wasm').subscribe();
 
 @Component({
   selector: 'app-scan',
-  imports: [],
+  imports: [NgxScannerQrcodeModule, JsonPipe, AsyncPipe],
   templateUrl: './scan.component.html',
   styleUrl: './scan.component.scss',
 })
