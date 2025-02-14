fb92faf3e5645bf3586a9690830f1b32b269afa6
iffam <iffaam@gmail.com>
2025-02-14 20:13:41 +0500

    feat: enhance layout component with dynamic menu and improved sidenav structure

--- ui/src/app/layout/layout.component.ts
+++ ui/src/app/layout/layout.component.ts
@@ -4,20 +4,60 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
 export class LayoutComponent {
   drawerMode: 'over' | 'side' = 'side';
   drawerOpened: boolean = true;
+  menuItems: any[] = [
+    {
+      title: 'Applications',
+      icon: 'apps',
+      link: 'applications',
+    },
+    {
+      title: 'ACL',
+      icon: 'lock',
+      link: 'acl',
+    },
+    {
+      title: 'Departments',
+      icon: 'business',
+      link: 'departments',
+    },
+  ];
 
   /**
    * Constructor
    */
   constructor() {}
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Public methods
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * Track by function for ngFor loops
+   *
+   * @param index
+   * @param item
+   */
+  trackByFn(index: number, item: any): any {
+    return item.id || index;
+  }
 }
