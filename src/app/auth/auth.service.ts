3aca1d4c6ab1a9b10b1727186d1f8e4c8bd82241
iffam <iffaam@gmail.com>
2025-02-15 17:03:45 +0500

    refactor: simplify signInUsingToken method by removing commented code

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, of, switchMap, throwError } from 'rxjs';
+import { Observable, of, switchMap, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { UserService } from '../modules/users/users.service';
 import { AuthUtils } from './auth.utils';
@@ -62,38 +62,39 @@ export class AuthService {
    * Sign in using the access token
    */
   signInUsingToken(): Observable<any> {
-    // Sign in using the token
-    return this._httpClient
-      .post('api/auth/sign-in-with-token', {
-        accessToken: this.accessToken,
-      })
-      .pipe(
-        catchError(() =>
-          // Return false
-          of(false)
-        ),
-        switchMap((response: any) => {
-          // Replace the access token with the new one if it's available on
-          // the response object.
-          //
-          // This is an added optional step for better security. Once you sign
-          // in using the token, you should generate a new one on the server
-          // side and attach it to the response object. Then the following
-          // piece of code can replace the token with the refreshed one.
-          if (response.accessToken) {
-            this.accessToken = response.accessToken;
-          }
+    return of(true);
+    // // Sign in using the token
+    // return this._httpClient
+    //   .post('api/auth/sign-in-with-token', {
+    //     accessToken: this.accessToken,
+    //   })
+    //   .pipe(
+    //     catchError(() =>
+    //       // Return false
+    //       of(false)
+    //     ),
+    //     switchMap((response: any) => {
+    //       // Replace the access token with the new one if it's available on
+    //       // the response object.
+    //       //
+    //       // This is an added optional step for better security. Once you sign
+    //       // in using the token, you should generate a new one on the server
+    //       // side and attach it to the response object. Then the following
+    //       // piece of code can replace the token with the refreshed one.
+    //       if (response.accessToken) {
+    //         this.accessToken = response.accessToken;
+    //       }
 
-          // Set the authenticated flag to true
-          this._authenticated = true;
+    //       // Set the authenticated flag to true
+    //       this._authenticated = true;
 
-          // Store the user on the user service
-          this._userService.authUser = response.user;
+    //       // Store the user on the user service
+    //       this._userService.authUser = response.user;
 
-          // Return true
-          return of(true);
-        })
-      );
+    //       // Return true
+    //       return of(true);
+    //     })
+    //   );
   }
 
   /**
