4dd0bec6f7c5a080b229f9516825208f58554b9d
iffam <iffaam@gmail.com>
2025-02-24 21:15:10 +0500

    feat: add permit management functionality with loading state and pagination support

--- src/app/modules/permits/permits.service.ts
+++ src/app/modules/permits/permits.service.ts
@@ -1,12 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, of, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, Observable, of, switchMap, tap, throwError } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { MyPermit, ScanData } from './permits.types';
 
 @Injectable({ providedIn: 'root' })
 export class PermitService {
   private _myPermit: BehaviorSubject<MyPermit | null> = new BehaviorSubject<MyPermit | null>(null);
+  private _permits: BehaviorSubject<any> = new BehaviorSubject<any[]>([]);
+  private _permitLoading: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private _pagination: BehaviorSubject<any> = new BehaviorSubject(null);
 
   /**
    * Constructor
@@ -24,9 +27,59 @@ export class PermitService {
     return this._myPermit.asObservable();
   }
 
+  /**
+   * Getter for applicatrion
+   */
+  get permits$(): Observable<any[]> {
+    return this._permits.asObservable();
+  }
+
+  /**
+   * Getter for applicatrion loading
+   */
+  get permitLoading$(): Observable<boolean> {
+    return this._permitLoading.asObservable();
+  }
+
+  /**
+   * Getter for pagination
+   */
+  get pagination$(): Observable<any> {
+    return this._pagination.asObservable();
+  }
   // -----------------------------------------------------------------------------------------------------
   // @ Public methods
   // -----------------------------------------------------------------------------------------------------
+
+  getPermits(page: string = '1', filter: string = 'all'): Observable<any> {
+    // Execute the visitors loading with true
+    this._permitLoading.next(true);
+
+    return this._httpClient
+      .get<any>(`${environment.apiUrl}/permits`, {
+        params: {
+          page,
+          filter,
+        },
+      })
+      .pipe(
+        tap((response: any) => {
+          this._permits.next(response.data);
+          this._pagination.next(response.pagination);
+          this._permitLoading.next(false);
+        }),
+        switchMap((response) => {
+          if (response.data === null) {
+            return throwError({
+              message: 'Requested page is not available!',
+              pagination: response.data.pagination,
+            });
+          }
+
+          return of(response);
+        })
+      );
+  }
 
   /**
    * Get the my permit
